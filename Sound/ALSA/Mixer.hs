da921101932a157ccfacb46f438573a180480b5f
Thomas Tuegel <ttuegel@gmail.com>
2011-01-04 21:22:37 -0600

    New version of 'controls'.
    Ignore-this: e3910b9909b049e4a75800466db35dac
    
    darcs-hash:20110105032237-30370-f46b9d01f4cb83dc591f8b6d286eb6c99209aa84

--- Sound/ALSA/Mixer.hs
+++ Sound/ALSA/Mixer.hs
@@ -68,80 +68,112 @@ common :: Either a (Maybe a, Maybe a) -> Maybe a
 common (Left x) = Just x
 common (Right _) = Nothing
 
+mkSwitch :: SimpleElement -> IO (Either Switch (Maybe Switch, Maybe Switch))
+mkSwitch se = do
+    hasPlayChan <- mapM (hasPlaybackChannel se) allChannels
+    hasCaptChan <- mapM (hasCaptureChannel se) allChannels
+    let pChans = map fst $ filter snd $ zip allChannels hasPlayChan
+        cChans = map fst $ filter snd $ zip allChannels hasCaptChan
+    hasComSw <- hasCommonSwitch se
+    hasPlaySw <- hasPlaybackSwitch se
+    hasPlaySwJ <- hasPlaybackSwitchJoined se
+    hasCaptSw <- hasCaptureSwitch se
+    hasCaptSwJ <- hasCaptureSwitchJoined se
+    return $ case hasComSw of
+        True -> Left $ case hasPlaySwJ of
+            True -> comJoinedSwitch pChans
+            False -> comPerChannelSwitch pChans
+        False ->
+            let playSw = case hasPlaySw of
+                    False -> Nothing
+                    True -> Just $ case hasPlaySwJ of
+                        True -> playJoinedSwitch pChans
+                        False -> playPerChannelSwitch pChans
+                captSw = case hasCaptSw of
+                    False -> Nothing
+                    True -> Just $ case hasCaptSwJ of
+                        True -> captJoinedSwitch cChans
+                        False -> captPerChannelSwitch cChans
+            in Right (playSw, captSw)
+  where joined fGet fSet chans =
+            Joined { getJoined = fGet se (head chans)
+                   , setJoined = fSet se (head chans)
+                   }
+        perChannel fGet fSet chans =
+            PerChannel { getPerChannel = liftM (zip chans)
+                            $ mapM (fGet se) chans
+                       , setPerChannel = mapM_ (uncurry (fSet se))
+                       }
+        comJoinedSwitch = joined getPlaybackSwitch setPlaybackSwitch
+        comPerChannelSwitch = perChannel getPlaybackSwitch setPlaybackSwitch
+        playJoinedSwitch = comJoinedSwitch
+        playPerChannelSwitch = comPerChannelSwitch
+        captJoinedSwitch = joined getCaptureSwitch setCaptureSwitch
+        captPerChannelSwitch = perChannel getCaptureSwitch setCaptureSwitch
+
+mkVolume :: SimpleElement -> IO (Either Volume (Maybe Volume, Maybe Volume))
+mkVolume se = do
+    hasPlayChan <- mapM (hasPlaybackChannel se) allChannels
+    hasCaptChan <- mapM (hasCaptureChannel se) allChannels
+    let pChans = map fst $ filter snd $ zip allChannels hasPlayChan
+        cChans = map fst $ filter snd $ zip allChannels hasCaptChan
+    hasComV <- hasCommonVolume se
+    hasPlayV <- hasPlaybackVolume se
+    hasPlayVJ <- hasPlaybackVolumeJoined se
+    hasCaptV <- hasCaptureVolume se
+    hasCaptVJ <- hasCaptureVolumeJoined se
+    return $ case hasComV of
+        True -> Left $ playVolume { value = case hasPlayVJ of
+            True -> comJoinedVolume pChans
+            False -> comPerChannelVolume pChans }
+        False ->
+            let playVol = case hasPlayV of
+                    False -> Nothing
+                    True -> Just $ playVolume { value = case hasPlayVJ of
+                        True -> playJoinedVolume pChans
+                        False -> playPerChannelVolume pChans }
+                captVol = case hasCaptV of
+                    False -> Nothing
+                    True -> Just $ captVolume { value = case hasCaptVJ of
+                        True -> captJoinedVolume cChans
+                        False -> captPerChannelVolume cChans }
+            in Right (playVol, captVol)
+  where joined fGet fSet chans =
+            Joined { getJoined = fGet se (head chans)
+                   , setJoined = fSet se (head chans)
+                   }
+        perChannel fGet fSet chans =
+            PerChannel { getPerChannel = liftM (zip chans)
+                            $ mapM (fGet se) chans
+                       , setPerChannel = mapM_ (uncurry (fSet se))
+                       }
+        playVolume = Volume { getRange = getPlaybackVolumeRange se
+                            , setRange = setPlaybackVolumeRange se
+                            , value = undefined
+                            }
+        captVolume = Volume { getRange = getCaptureVolumeRange se
+                            , setRange = setCaptureVolumeRange se
+                            , value = undefined
+                            }
+        comJoinedVolume = joined getPlaybackVolume setPlaybackVolume
+        comPerChannelVolume = perChannel getPlaybackVolume setPlaybackVolume
+        playJoinedVolume = comJoinedVolume
+        playPerChannelVolume = comPerChannelVolume
+        captJoinedVolume = joined getCaptureVolume setCaptureVolume
+        captPerChannelVolume = perChannel getCaptureVolume setCaptureVolume
+
 controls :: Mixer -> IO [Control]
 controls mix = do
     es <- elements mix
     forM es $ \(idN, se) -> do
         n <- getName idN
         i <- getIndex idN
-        hasPChan <- mapM (hasPlaybackChannel se) allChannels
-        hasCChan <- mapM (hasCaptureChannel se) allChannels
-        let pChans = map fst $ filter snd $ zip allChannels hasPChan
-            cChans = map fst $ filter snd $ zip allChannels hasCChan
-        hasCSw <- hasCommonSwitch se
-        sw <- case hasCSw of
-            True -> do
-                hasJSw <- hasPlaybackSwitchJoined se
-                return $ Left $ case hasJSw of
-                    True -> commonJoinedSwitch se pChans
-                    False -> commonPerChannelSwitch se pChans
-            False -> do
-                hasPSw <- hasPlaybackSwitch se
-                pSwitch <- case hasPSw of
-                    False -> return Nothing
-                    True -> do
-                        hasPJSw <- hasPlaybackSwitchJoined se
-                        return $ Just $ case hasPJSw of
-                            True -> playbackJoinedSwitch se pChans
-                            False -> playbackPerChannelSwitch se pChans
-                hasCSw <- hasCaptureSwitch se
-                cSwitch <- case hasCSw of
-                    False -> return Nothing
-                    True -> do
-                        hasCJSw <- hasCaptureSwitchJoined se
-                        return $ Just $ case hasCJSw of
-                            True -> captureJoinedSwitch se cChans
-                            False -> capturePerChannelSwitch se cChans
-                return $ Right (pSwitch, cSwitch)
-        hasCV <- hasCommonVolume se
-        v <- case hasCV of
-            True -> do
-                hasJV <- hasPlaybackVolumeJoined se
-                let v = case hasJV of
-                            True -> commonJoinedVolume se pChans
-                            False -> commonPerChannelVolume se pChans
-                return $ Left $ Volume { getRange = getPlaybackVolumeRange se
-                                       , setRange = setPlaybackVolumeRange se
-                                       , value = v
-                                       }
-            False -> do
-                hasPV <- hasPlaybackVolume se
-                pVolume <- case hasPV of
-                    False -> return Nothing
-                    True -> do
-                        hasPJV <- hasPlaybackVolumeJoined se
-                        return $ Just $
-                            let v = case hasPJV of
-                                        True -> playbackJoinedVolume se pChans
-                                        False -> playbackPerChannelVolume se pChans
-                            in Volume { setRange = setPlaybackVolumeRange se
-                                      , getRange = getPlaybackVolumeRange se
-                                      , value = v
-                                      }
-                hasCV <- hasCaptureVolume se
-                cVolume <- case hasCV of
-                    False -> return Nothing
-                    True -> do
-                        hasCJV <- hasCaptureVolumeJoined se
-                        return $ Just $
-                            let v = case hasCJV of
-                                        True -> captureJoinedVolume se cChans
-                                        False -> capturePerChannelVolume se cChans
-                            in Volume { setRange = setCaptureVolumeRange se
-                                      , getRange = getCaptureVolumeRange se
-                                      , value = v
-                                      }
-                return $ Right (pVolume, cVolume)
+        sw <- mkSwitch se
+        v <- mkVolume se
+        hasPlayChan <- mapM (hasPlaybackChannel se) allChannels
+        hasCaptChan <- mapM (hasCaptureChannel se) allChannels
+        let pChans = map fst $ filter snd $ zip allChannels hasPlayChan
+            cChans = map fst $ filter snd $ zip allChannels hasCaptChan
         return $! Control { selem = se
                           , seId = idN
                           , name = n
@@ -150,28 +182,3 @@ controls mix = do
                           , volume = v
                           , channels = (pChans, cChans)
                           }
-  where joined fGet fSet se chans =
-            Joined { getJoined = fGet se (head chans)
-                   , setJoined = fSet se (head chans)
-                   }
-        perChannel fGet fSet se chans =
-            PerChannel { getPerChannel = liftM (zip chans)
-                            $ mapM (fGet se) chans
-                       , setPerChannel = mapM_ (uncurry (fSet se))
-                       }
-        commonJoinedSwitch = joined getPlaybackSwitch setPlaybackSwitch
-        commonPerChannelSwitch =
-            perChannel getPlaybackSwitch setPlaybackSwitch
-        commonJoinedVolume = joined getPlaybackVolume setPlaybackVolume
-        commonPerChannelVolume =
-            perChannel getPlaybackVolume setPlaybackVolume
-        playbackJoinedSwitch = commonJoinedSwitch
-        playbackPerChannelSwitch = commonPerChannelSwitch
-        playbackJoinedVolume = commonJoinedVolume
-        playbackPerChannelVolume = commonPerChannelVolume
-        captureJoinedSwitch = joined getCaptureSwitch setCaptureSwitch
-        capturePerChannelSwitch =
-            perChannel getCaptureSwitch setCaptureSwitch
-        captureJoinedVolume = joined getCaptureVolume setCaptureVolume
-        capturePerChannelVolume =
-            perChannel getCaptureVolume setCaptureVolume
