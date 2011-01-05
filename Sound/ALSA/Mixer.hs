0c95ccf701c0dee8c12317aa8fc5fae4aec16464
Thomas Tuegel <ttuegel@gmail.com>
2011-01-04 20:35:39 -0600

    New simplified API.
    Ignore-this: 8100733e518aa8587724976c871f201f
    
    darcs-hash:20110105023539-30370-75f3915746c68b80c0873e6760bf1de8eaee2158

--- Sound/ALSA/Mixer.hs
+++ Sound/ALSA/Mixer.hs
@@ -1,11 +1,19 @@
 module Sound.ALSA.Mixer
     ( Volume(..)
     , Control()
-    , contents
     , controls
     , channels
     , name
     , index
+    , switch
+    , volume
+    , common
+    , playback
+    , capture
+    , Switch()
+    , PerChannel(..)
+    , getChannel
+    , setChannel
     , getMixerByName
     , Channel(..)
     ) where
@@ -21,7 +29,44 @@ data Control = Control { selem :: SimpleElement
                        , seId :: SimpleElementId
                        , index :: Integer
                        , name :: String
+                       , switch :: Either Switch (Maybe Switch, Maybe Switch)
+                       , volume :: Either Volume (Maybe Volume, Maybe Volume)
+                       , channels :: ([Channel], [Channel])
                        }
+
+type Switch = PerChannel Bool
+
+data PerChannel e = Joined { getJoined :: IO e
+                           , setJoined :: e -> IO ()
+                           }
+                  | PerChannel { getPerChannel :: IO [(Channel, e)]
+                               , setPerChannel :: [(Channel, e)] -> IO ()
+                               }
+
+data Volume = Volume { getRange :: IO (Integer, Integer)
+                     , setRange :: (Integer, Integer) -> IO ()
+                     , value :: PerChannel Integer
+                     }
+
+getChannel :: PerChannel x -> Channel -> IO (Maybe x)
+getChannel j@(Joined f _) _ = liftM Just f
+getChannel p@(PerChannel f _) c = liftM (lookup c) f
+
+setChannel :: PerChannel x -> Channel -> x -> IO ()
+setChannel j@(Joined _ f) _ v = f v
+setChannel p@(PerChannel _ f) c v = f [(c, v)]
+
+playback :: Either a (Maybe a, Maybe a) -> Maybe a
+playback (Left _) = Nothing
+playback (Right (x, _)) = x
+
+capture :: Either a (Maybe a, Maybe a) -> Maybe a
+capture (Left _) = Nothing
+capture (Right (_, x)) = x
+
+common :: Either a (Maybe a, Maybe a) -> Maybe a
+common (Left x) = Just x
+common (Right _) = Nothing
 
 controls :: Mixer -> IO [Control]
 controls mix = do
@@ -29,150 +74,104 @@ controls mix = do
     forM es $ \(idN, se) -> do
         n <- getName idN
         i <- getIndex idN
+        hasPChan <- mapM (hasPlaybackChannel se) allChannels
+        hasCChan <- mapM (hasCaptureChannel se) allChannels
+        let pChans = map fst $ filter snd $ zip allChannels hasPChan
+            cChans = map fst $ filter snd $ zip allChannels hasCChan
+        hasCSw <- hasCommonSwitch se
+        sw <- case hasCSw of
+            True -> do
+                hasJSw <- hasPlaybackSwitchJoined se
+                return $ Left $ case hasJSw of
+                    True -> commonJoinedSwitch se pChans
+                    False -> commonPerChannelSwitch se pChans
+            False -> do
+                hasPSw <- hasPlaybackSwitch se
+                pSwitch <- case hasPSw of
+                    False -> return Nothing
+                    True -> do
+                        hasPJSw <- hasPlaybackSwitchJoined se
+                        return $ Just $ case hasPJSw of
+                            True -> playbackJoinedSwitch se pChans
+                            False -> playbackPerChannelSwitch se pChans
+                hasCSw <- hasCaptureSwitch se
+                cSwitch <- case hasCSw of
+                    False -> return Nothing
+                    True -> do
+                        hasCJSw <- hasCaptureSwitchJoined se
+                        return $ Just $ case hasCJSw of
+                            True -> captureJoinedSwitch se cChans
+                            False -> capturePerChannelSwitch se cChans
+                return $ Right (pSwitch, cSwitch)
+        hasCV <- hasCommonVolume se
+        v <- case hasCV of
+            True -> do
+                hasJV <- hasPlaybackVolumeJoined se
+                let v = case hasJV of
+                            True -> commonJoinedVolume se pChans
+                            False -> commonPerChannelVolume se pChans
+                return $ Left $ Volume { getRange = getPlaybackVolumeRange se
+                                       , setRange = setPlaybackVolumeRange se
+                                       , value = v
+                                       }
+            False -> do
+                hasPV <- hasPlaybackVolume se
+                pVolume <- case hasPV of
+                    False -> return Nothing
+                    True -> do
+                        hasPJV <- hasPlaybackVolumeJoined se
+                        return $ Just $
+                            let v = case hasPJV of
+                                        True -> playbackJoinedVolume se pChans
+                                        False -> playbackPerChannelVolume se pChans
+                            in Volume { setRange = setPlaybackVolumeRange se
+                                      , getRange = getPlaybackVolumeRange se
+                                      , value = v
+                                      }
+                hasCV <- hasCaptureVolume se
+                cVolume <- case hasCV of
+                    False -> return Nothing
+                    True -> do
+                        hasCJV <- hasCaptureVolumeJoined se
+                        return $ Just $
+                            let v = case hasCJV of
+                                        True -> captureJoinedVolume se cChans
+                                        False -> capturePerChannelVolume se cChans
+                            in Volume { setRange = setCaptureVolumeRange se
+                                      , getRange = getCaptureVolumeRange se
+                                      , value = v
+                                      }
+                return $ Right (pVolume, cVolume)
         return $! Control { selem = se
                           , seId = idN
                           , name = n
                           , index = i
+                          , switch = sw
+                          , volume = v
+                          , channels = (pChans, cChans)
                           }
-
-data Mode = Playback | Capture
-    deriving (Read, Show, Eq)
-
-data Element e = Element { get :: IO e
-                         , set :: e -> IO ()
-                         }
-
--- | This function does not check that the channel exists before constructing
--- an 'Element'.  The caller must take care that 'mkSwitch' is not called
--- for a non-existent channel.
-mkSwitch :: Control -> (Mode, Channel) -> IO (Maybe (Element Bool))
-mkSwitch cn (m, ch) = do
-    hasSw <- hasSwitch s
-    case hasSw of
-        False -> return Nothing
-        True -> return $ Just Element { get = getSwitch s ch, set = setSwitch s ch }
-  where s = selem cn
-        hasSwitch :: SimpleElement -> IO Bool
-        hasSwitch = case m of
-                        Playback -> hasPlaybackSwitch
-                        Capture -> hasCaptureSwitch
-        getSwitch :: SimpleElement -> Channel -> IO Bool
-        getSwitch = case m of
-                        Playback -> getPlaybackSwitch
-                        Capture -> getCaptureSwitch
-        setSwitch :: SimpleElement -> Channel -> Bool -> IO ()
-        setSwitch = case m of
-                        Playback -> setPlaybackSwitch
-                        Capture -> setCaptureSwitch
-
-mkVolume :: Control -> (Mode, Channel) -> Bool -> IO (Maybe (Element Volume))
-mkVolume cn (m, ch) db = do
-    hasV <- hasVolume s
-    case hasV of
-        False -> return Nothing
-        True -> case db of
-            False -> return ret 
-            True -> do
-                dBv <- catchInvalidAsNothing $ getVolume s ch
-                case dBv of
-                    Nothing -> return Nothing
-                    Just _ -> return ret
-  where s = selem cn
-        ret = Just Element { get = getVolume s ch
-                           , set = setVolume s ch
-                           }
-        hasVolume = case m of
-                        Playback -> hasPlaybackVolume
-                        Capture -> hasCaptureVolume
-        getVolume = case m of
-                        Playback -> getPlayback db
-                        Capture -> getCapture db
-        setVolume = case m of
-                        Playback -> setPlayback db
-                        Capture -> setCapture db
-      
-data Volume = Volume { value :: Integer
-                     , range :: (Integer, Integer)
-                     }
-    deriving (Read, Show, Eq)
-
-getPlayback :: Bool -> SimpleElement -> Channel -> IO Volume
-getPlayback db s ch = do
-    v <- getVolume s ch
-    (lo, hi) <- getRange s
-    return Volume { value = v, range = (lo, hi) }
-  where getVolume = case db of
-                        False -> getPlaybackVolume
-                        True -> getPlaybackDb
-        getRange = case db of
-                       False -> getPlaybackVolumeRange
-                       True -> getPlaybackDbRange
-
-
-getCapture :: Bool -> SimpleElement -> Channel -> IO Volume
-getCapture db s ch = do
-    v <- getVolume s ch
-    (lo, hi) <- getRange s
-    return Volume { value = v, range = (lo, hi) }
-  where getVolume = case db of
-                        False -> getCaptureVolume
-                        True -> getCaptureDb
-        getRange = case db of
-                       False -> getCaptureVolumeRange
-                       True -> getCaptureDbRange
-
-setPlayback :: Bool -> SimpleElement -> Channel -> Volume -> IO ()
-setPlayback db s ch v = do
-    setVolume s ch $ value v
-    --setRange s ch $ range v
-  where setVolume = case db of
-                        False -> setPlaybackVolume
-                        True -> setPlaybackDb
-        --setRange = case db of
-                       --False -> setPlaybackVolumeRange
-                       --True -> setPlaybackDbRange
-
-setCapture :: Bool -> SimpleElement -> Channel -> Volume -> IO ()
-setCapture db s ch v = do
-    setVolume s ch $ value v
-    --setRange s ch $ range v
-  where setVolume = case db of
-                        False -> setCaptureVolume
-                        True -> setCaptureDb
-        --setRange = case db of
-                       --False -> setCaptureVolumeRange
-                       --True -> setCaptureDbRange
-
-data Content = Content { mode :: Mode
-                       , channel :: Channel
-                       , switch :: Maybe (Element Bool)
-                       , volume :: Maybe (Element Volume)
-                       , dB :: Maybe (Element Volume)
+  where joined fGet fSet se chans =
+            Joined { getJoined = fGet se (head chans)
+                   , setJoined = fSet se (head chans)
+                   }
+        perChannel fGet fSet se chans =
+            PerChannel { getPerChannel = liftM (zip chans)
+                            $ mapM (fGet se) chans
+                       , setPerChannel = mapM_ (uncurry (fSet se))
                        }
-
-channels :: Control -> IO [(Mode, Channel)]
-channels con = do
-    hasPChan <- mapM (hasPlaybackChannel s) allChannels
-    hasCChan <- mapM (hasCaptureChannel s) allChannels
-    let pChans = map fst $ filter snd $ zip allChannels hasPChan
-        cChans = map fst $ filter snd $ zip allChannels hasCChan
-        chans = (map (\x -> (Playback, x)) pChans) ++
-                (map (\x -> (Capture, x)) cChans)
-    return chans
-  where s = selem con
-catchInvalidAsNothing a = catchErrno (Errno 22) (liftM Just a) (return Nothing)
-
-contents :: Control -> IO [Content]
-contents c = do
-    chans <- channels c
-    mapM worker chans
-  where worker (m, ch) = do
-            sw <- mkSwitch c (m, ch)
-            v <- mkVolume c (m, ch) False
-            d <- mkVolume c (m, ch) True
-            return Content { mode = m
-                           , channel = ch
-                           , switch = sw
-                           , volume = v
-                           , dB = d
-                           }
+        commonJoinedSwitch = joined getPlaybackSwitch setPlaybackSwitch
+        commonPerChannelSwitch =
+            perChannel getPlaybackSwitch setPlaybackSwitch
+        commonJoinedVolume = joined getPlaybackVolume setPlaybackVolume
+        commonPerChannelVolume =
+            perChannel getPlaybackVolume setPlaybackVolume
+        playbackJoinedSwitch = commonJoinedSwitch
+        playbackPerChannelSwitch = commonPerChannelSwitch
+        playbackJoinedVolume = commonJoinedVolume
+        playbackPerChannelVolume = commonPerChannelVolume
+        captureJoinedSwitch = joined getCaptureSwitch setCaptureSwitch
+        capturePerChannelSwitch =
+            perChannel getCaptureSwitch setCaptureSwitch
+        captureJoinedVolume = joined getCaptureVolume setCaptureVolume
+        capturePerChannelVolume =
+            perChannel getCaptureVolume setCaptureVolume
