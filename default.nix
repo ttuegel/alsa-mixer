025d339de1b257b47763097b8b18c7334608b8db
Thomas Tuegel <ttuegel@gmail.com>
2014-04-15 09:45:38 -0500

    default.nix: Upgrade to GHC 7.8.2

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
 { pkgs ? (import <nixpkgs> {}) }:
 
 let
-  inherit (pkgs) haskellPackages;
+  #inherit (pkgs) haskellPackages;
+  haskellPackages = pkgs.haskellPackages_ghc782;
+  c2hs = pkgs.haskellPackages_ghc763.c2hs; # Does not build yet with GHC 7.8
 in
 with haskellPackages;
 cabal.mkDerivation (self: {
@@ -9,7 +11,7 @@ cabal.mkDerivation (self: {
   version = "0.2.0";
   src = ./.;
   buildDepends = [ alsaCore ];
-  buildTools = [ c2hs ];
+  buildTools = [ c2hs cabalInstall ];
   extraLibraries = [ pkgs.alsaLib ];
   meta = {
     description = "Bindings to the ALSA simple mixer API";
